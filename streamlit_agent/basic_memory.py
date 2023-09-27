26ff1542ff6d288bb8224557bbba92e53dfc986d
Erik Nygren <baerenjesus@gmail.com>
2023-09-27 20:44:11 +0000

    added avatars

--- streamlit_agent/basic_memory.py
+++ streamlit_agent/basic_memory.py
@@ -53,11 +53,11 @@ for msg in msgs.messages:
 
 # If user inputs a new prompt, generate and draw a new response
 if prompt := st.chat_input():
-    st.chat_message("human").write(prompt)
+    st.chat_message("human",avatar="🧑‍⚖️").write(prompt)
     msgs.add_user_message(prompt)
     # Note: new messages are saved to history automatically by Langchain during run
     response = llm_chain.predict(instruction=instruction,history = msgs,human_input=prompt)
-    st.chat_message("ai").write(response)
+    st.chat_message("ai,avatar="🤖").write(response)
     msgs.add_ai_message(response)
 
 if st.button(label="Clear Chat"):
