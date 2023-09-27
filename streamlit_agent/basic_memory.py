f15ab5995f66e16be529ce4d0821f994684f7404
Erik Nygren <baerenjesus@gmail.com>
2023-09-27 20:51:16 +0000

    update to avatar

--- streamlit_agent/basic_memory.py
+++ streamlit_agent/basic_memory.py
@@ -64,8 +64,7 @@ if st.button(label="Clear Chat"):
     msgs.clear()
     # Render current messages from StreamlitChatMessageHistory
     for msg in msgs.messages:
-        print(msg.type)
-        if msg.type == "student":
+        if msg.type == "human":
             st.chat_message(msg.type,avatar="🧑‍⚖️" ).write(msg.content)
         else:
             st.chat_message(msg.type,avatar="🤖" ).write(msg.content)
